45ebf3bdaf13f86344caa20f6b01cb7b96d9ebb5
luachristian <clualua@ycp.edu>
2023-03-17 01:08:29 -0400

    Added all changes to the Lab

--- MoveTheSquare/src/edu/ycp/cs320/movethesquare/controllers/GameController.java
+++ MoveTheSquare/src/edu/ycp/cs320/movethesquare/controllers/GameController.java
@@ -1,33 +1,62 @@
 package edu.ycp.cs320.movethesquare.controllers;
 
+import edu.ycp.cs320.movethesquare.model.Circle;
 import edu.ycp.cs320.movethesquare.model.Game;
-import edu.ycp.cs320.movethesquare.model.Square;
-
 public class GameController {
-	public void computeSquareMoveDirection(Game game, Square square, double mouseX, double mouseY) {
-		if (mouseX >= 0 && mouseX < game.getWidth() && mouseY >= 0 && mouseY < game.getHeight()) {
-			double dx = mouseX - (square.getX() + square.getWidth()/2);
-			double dy = mouseY - (square.getY() + square.getHeight()/2);
+	public void computeSquareMoveDirection(Game game, Circle circle, double mouseX, double mouseY) {
+		if (mouseX > 0 && mouseX < game.getWidth() && mouseY > 0 && mouseY < game.getHeight()) {
+			double dx = mouseX - (circle.getX() + circle.getWidth()/2);
+			double dy = mouseY - (circle.getY() + circle.getHeight()/2);
 			
 			double moveX = 0, moveY = 0;
-			if (dx > 0) {
-				moveX = Game.MOVE_DIST;
-			} else {
-				moveX = -Game.MOVE_DIST;
-			}
-			if (dy > 0) {
-				moveY = Game.MOVE_DIST;
-			} else {
-				moveY = -Game.MOVE_DIST;
-			}
 			
-			game.setSquareDx(moveX);
-			game.setSquareDy(moveY);
+				
+				
+					if (dx >= 0) {
+						moveX = Game.MOVE_DIST;
+					} else {
+						moveX = -Game.MOVE_DIST;
+					}
+					if (dy >= 0) {
+						moveY = Game.MOVE_DIST;
+					} else {
+						moveY = -Game.MOVE_DIST;
+					}
+					
+			
+				game.setCircleDx(moveX);
+				game.setCircleDy(moveY);
 		}
+		
 	}
 
-	public void moveSquare(Game model, Square square) {
-		square.setX(square.getX() + model.getSquareDx());
-		square.setY(square.getY() + model.getSquareDy());
+	public void moveSquare(Game model, Circle circle) {
+		
+			//top
+		if((circle.getX() + model.getCircleDx() > 0) && (circle.getX() + model.getCircleDx() < model.getWidth())) 
+		{
+			circle.setX(circle.getX() + model.getCircleDx());
+		}
+		else {
+			circle.setX(0);
+		}
+		
+		
+		
+			//left
+		if((circle.getY() + model.getCircleDy() > 0) && (circle.getY() + model.getCircleDy() < model.getHeight()))
+		{
+			circle.setY(circle.getY() + model.getCircleDy());
+		}
+		else {
+			circle.setY(0);
+		}
+	
+			//right
+		if((circle.getY() + model.getCircleDy() > 0) && (circle.getY() + model.getCircleDy() < model.getHeight()) && (circle.getX() < model.getWidth() - circle.getDiameter())){
+			circle.setY(circle.getY() + model.getCircleDy());
+		}
+		
 	}
 }
+	
